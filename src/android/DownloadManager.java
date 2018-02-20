496d5328c244218f1986751c397152d5b874b286
tolgatasci <tolgatasci1@gmail.com>
2018-02-21 00:54:35 +0300

    Message

--- src/android/DownloadManager.java
+++ src/android/DownloadManager.java
@@ -23,13 +23,14 @@ public class DownloadManager extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("download")) {
             String message = args.getString(0);
-            this.startDownload(message, callbackContext);
+	    String file_name = 	args.getString(1);
+            this.startDownload(message,file_name, callbackContext);
             return true;
         }
         return false;
     }
 
-    private void startDownload(String message, CallbackContext callbackContext) {
+    private void startDownload(String message, String file_name, CallbackContext callbackContext) {
         if (message != null && message.length() > 0) {
             String filename = message.substring(message.lastIndexOf("/")+1, message.length());
             try {
@@ -41,20 +42,20 @@ public class DownloadManager extends CordovaPlugin {
             android.app.DownloadManager downloadManager = (android.app.DownloadManager) cordova.getActivity().getApplicationContext().getSystemService(Context.DOWNLOAD_SERVICE);            
             Uri Download_Uri = Uri.parse(message);
             android.app.DownloadManager.Request request = new android.app.DownloadManager.Request(Download_Uri);
-            //Restrict the types of networks over which this download may proceed.
+            
             request.setAllowedNetworkTypes(android.app.DownloadManager.Request.NETWORK_WIFI | android.app.DownloadManager.Request.NETWORK_MOBILE);
-            //Set whether this download may proceed over a roaming connection.
+          
             request.setAllowedOverRoaming(false);
-            //Set the title of this download, to be displayed in notifications (if enabled).
-            request.setTitle(filename);
-            //Set a description of this download, to be displayed in notifications (if enabled)
-            request.setDescription("DataSync File Download.");
-            //Set the local destination for the downloaded file to a path within the application's external files directory            
-            request.setDestinationInExternalFilesDir(cordova.getActivity().getApplicationContext(), Environment.DIRECTORY_DOWNLOADS, filename);
-            //Set visiblity after download is complete
+
+            request.setTitle(file_name);
+
+            request.setDescription("Cici Müzik");
+
+            request.setDestinationInExternalFilesDir(cordova.getActivity().getApplicationContext(), Environment.DIRECTORY_MUSIC, file_name);
+
             request.setNotificationVisibility(android.app.DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
             long downloadReference = downloadManager.enqueue(request);
-            callbackContext.success(message);
+            callbackContext.success(Environment.DIRECTORY_MUSIC+'/'+file_name);
         } else {
             callbackContext.error("Expected one non-empty string argument.");
         }
